71430261861a7b7048e79d15e6ce25723507084a
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-23 23:57:15 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
   def keys_of(*arguments)
     array = []
     arguments.each do |x|
-      self.each do |k, v|
+      x.each do |k, v|
         if v == x
           array << k
         end
