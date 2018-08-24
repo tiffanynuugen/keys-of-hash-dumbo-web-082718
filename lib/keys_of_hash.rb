49277828bab92eba3b573c58db5a53e33cbf6471
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-24 00:00:12 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    arguments.each do |x|
+      self.each do |k, v|
+        if v == x
+          array << k
+        end
+      end
+    end
+    array
   end
 end
